b87ab49d7e1c44d07a83338f2bd0338cd5f6de91
Luc Patiny <luc@patiny.com>
2022-08-30 13:57:04 +0200

    chore: fix types

--- docker/src/append/appendSDFStream.ts
+++ docker/src/append/appendSDFStream.ts
@@ -1,7 +1,5 @@
 import debugLibrary from 'debug';
 import { Molecule } from 'openchemlib';
-import pAll from 'p-all';
-import { cpus } from 'os';
 
 //@ts-expect-error sdf-parser is not typed
 import { iterator } from 'sdf-parser';
--- docker/src/db/insertInfo.ts
+++ docker/src/db/insertInfo.ts
@@ -1,10 +1,10 @@
 import { Database, Statement } from 'better-sqlite3';
 
-import { MoleculeInfo } from '../MoleculeInfo';
+import { InternalMoleculeInfo } from '../InternalMoleculeInfo';
 
 let stmt: Statement;
 
-export function insertInfo(info: MoleculeInfo, db: Database) {
+export function insertInfo(info: InternalMoleculeInfo, db: Database) {
   if (!stmt) {
     stmt = db.prepare(
       'INSERT INTO molecules VALUES (@idCode, @mf, @em, @mw, @noStereoID, @noStereoTautomerID, @logS, @logP, @acceptorCount, @donorCount, @rotatableBondCount, @stereoCenterCount, @polarSurfaceArea, @ssIndex)',
