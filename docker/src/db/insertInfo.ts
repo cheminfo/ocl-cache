c9a0541bd09452c05f2fbcc2c0354fbaae63dc30
Luc Patiny <luc@patiny.com>
2022-08-30 13:47:21 +0200

    chore: deal with error if idCode exists

--- docker/src/db/insertInfo.ts
+++ docker/src/db/insertInfo.ts
@@ -1,20 +1,18 @@
 import { Database, Statement } from 'better-sqlite3';
 
-import { InternalMoleculeInfo } from '../InternalMoleculeInfo';
+import { MoleculeInfo } from '../MoleculeInfo';
 
 let stmt: Statement;
 
-export function insertInfo(info: InternalMoleculeInfo, db: Database) {
-	if (!stmt) {
-		stmt = db.prepare(
-			'INSERT INTO molecules VALUES (@idCode, @mf, @em, @mw, @noStereoID, @noStereoTautomerID, @logS, @logP, @acceptorCount, @donorCount, @rotatableBondCount, @stereoCenterCount, @polarSurfaceArea, @ssIndex)',
-		);
-	}
-	try {
-		stmt.run(info);
-
-	} catch (e) {
-		console.log(e);
-		console.log('Existing idCode: ', info.idCode)
-	}
+export function insertInfo(info: MoleculeInfo, db: Database) {
+  if (!stmt) {
+    stmt = db.prepare(
+      'INSERT INTO molecules VALUES (@idCode, @mf, @em, @mw, @noStereoID, @noStereoTautomerID, @logS, @logP, @acceptorCount, @donorCount, @rotatableBondCount, @stereoCenterCount, @polarSurfaceArea, @ssIndex)',
+    );
+  }
+  try {
+    stmt.run(info);
+  } catch (e) {
+    console.log(`idCode already exists in the database: ${info.idCode}`);
+  }
 }
