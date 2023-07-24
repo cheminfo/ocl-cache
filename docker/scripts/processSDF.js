e208533dec051e46c917f527f93b63e2dc9ae325
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-07-24 16:55:18 +0200

    fix: renameSync to_process process

--- docker/scripts/processSDF.js
+++ docker/scripts/processSDF.js
@@ -1,4 +1,4 @@
-const { renameSync } = require('fs');
+const { renameSync, existsSync, mkdirSync } = require('fs');
 const { ReadStream } = require('fs');
 const { join } = require('path');
 
@@ -8,25 +8,32 @@ const { fileCollectionFromPath } = require('filelist-utils');
 
 const { appendSDFStream } = require('../lib/index.js');
 
-async function doAll() {
+ async function processSDF() {
   let wasWaiting = true;
   const sdfDir = join(__dirname, '../sdf/to_process');
   debug(`Checking for SDF files in: ${sdfDir}`);
   while (true) {
-    const fileList = await fileCollectionFromPath(sdfDir);
+    const fileList = await fileCollectionFromPath(sdfDir, { ungzip: { gzipExtensions: [] } });
     for (const file of fileList) {
       wasWaiting = false;
       debug(`Importing: ${file.name}`);
-      await appendSDFStream(ReadStream.fromWeb(file.stream()));
-      let filename = file.relativePath.replace(/\.zip\/.*$/, '.zip');
-      renameSync(filename, filename.replace('to_process', 'processed'));
+     debug(file)
+      await appendSDFStream(ReadStream.from(file.stream()));
+      let filePath=join(sdfDir,file.name);
+      if(!existsSync(sdfDir.replace('to_process', 'processed'))){
+        mkdirSync(sdfDir.replace('to_process', 'processed'));
+      
+      }
+      renameSync(`${filePath}`, `${filePath.replace('to_process', 'processed')}`);
+      
     }
     if (wasWaiting) {
       debug('Wating for new SDF files');
       wasWaiting = true;
     }
+
     await delay(10000);
   }
 }
 
-doAll();
+processSDF();
