530ec80b9ba06fb1928f67c3ee3534bd73bebc58
RicardoSilvestr <ricardom.silvestr@hes-so.ch>
2023-07-25 10:55:00 +0200

    fix: ssIndex conversion

--- docker/src/db/improveMoleculeInfo.ts
+++ docker/src/db/improveMoleculeInfo.ts
@@ -1,13 +1,14 @@
+//import Debug from 'debug';
+
 import { InternalMoleculeInfo } from '../InternalMoleculeInfo';
 import { MoleculeInfo } from '../MoleculeInfo';
-import Debug from 'debug';
 
-const debug = Debug('improveMoleculeInfo');
+//const debug = Debug('improveMoleculeInfo');
 
 export function improveMoleculeInfo(data: InternalMoleculeInfo): MoleculeInfo {
   return {
     ...data,
     atoms: JSON.parse(data.atoms),
-    ssIndex: Array.from(new Uint32Array(data.ssIndex)),
+    ssIndex: Array.from(new Int32Array(new Uint8Array(data.ssIndex).buffer)),
   };
 }
